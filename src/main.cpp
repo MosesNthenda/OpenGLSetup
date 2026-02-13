b5d9568a9ec1efc2723e6bc5173d5094684f9f88
MosesNthenda <bsc-com-18-23@unima.ac.mw>
2026-02-13 14:43:08 +0100

    full hello_window_clear program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
         glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 int main() {
@@ -15,6 +21,7 @@ int main() {
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if( window == NULL) {
+        
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -22,6 +29,7 @@ int main() {
     glfwMakeContextCurrent(window);
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
@@ -30,7 +38,17 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    //render loop
     while(!glfwWindowShouldClose(window)) {
+        
+        //input
+        processInput(window);
+
+        //rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
